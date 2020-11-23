1ab426778f67ac1b249282acb407f0f820d56f47
VictorneoX <victorrodrigues753@gmail.com>
2020-11-23 20:46:53 -0300

    Regular Expressions

--- index.js
+++ index.js
@@ -40,12 +40,22 @@ async function getCourses() {
     // and
 
     const courses = await Course
-        //.find({ author: 'Mosh', isPublished: true })
-        //.find({ price: { $gte: 10, $lte: 20 } })
-        //.find({ price: { $in: [10, 15, 20] } })
+        /*.find({ author: 'Mosh', isPublished: true })
+        .find({ price: { $gte: 10, $lte: 20 } })
+        .find({ price: { $in: [10, 15, 20] } })
         .find()
         .or([ { author: 'Mosh' }, { isPublished: true } ])
-        .and([  ])
+        .and([  ])*/
+
+        // Starts with Mosh
+        .find({ author: /^Mosh/ })
+
+        // ends with Hamedani
+        .find({ author: /Hamedani$/i })
+
+        // Contains Mosh
+        .find({ author: /.*Mosh.*/i })
+
         .limit(10)
         .sort({ name: 1 })
         .select({ name: 1, tags: 1 });
