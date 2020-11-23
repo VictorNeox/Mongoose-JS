36b99ea82b5ef91eae8f914da3320d922f44c61c
VictorneoX <victorrodrigues753@gmail.com>
2020-11-23 20:43:15 -0300

    Logical Query Operators

--- index.js
+++ index.js
@@ -36,10 +36,16 @@ async function getCourses() {
     // in
     // nin (not in)
 
+    // or
+    // and
+
     const courses = await Course
         //.find({ author: 'Mosh', isPublished: true })
         //.find({ price: { $gte: 10, $lte: 20 } })
-        .find({ price: { $in: [10, 15, 20] } })
+        //.find({ price: { $in: [10, 15, 20] } })
+        .find()
+        .or([ { author: 'Mosh' }, { isPublished: true } ])
+        .and([  ])
         .limit(10)
         .sort({ name: 1 })
         .select({ name: 1, tags: 1 });
